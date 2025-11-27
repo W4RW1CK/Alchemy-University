6cfd4432dd1c0d789ab8f10bf7504e4718d25f80
W4RW1CK <ejcv0403@protonmail.com>
2025-11-26 23:03:57 -0600

    Proof of Work Miner

---
+++ Ethereum Bootcamp/Blockchain Cryptography/Mining & Proof of Work/index3.js
@@ -0,0 +1,28 @@
+const SHA256 = require('crypto-js/sha256');
+const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+    mempool.push(transaction);
+}
+
+function mine() {
+    const newBlock = {
+        id: blocks.length,
+    };
+    let hash = SHA256(JSON.stringify(newBlock))
+    blocks.push({newBlock, hash});
+    
+}
+
+module.exports = {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction, 
+    mine, 
+    blocks,
+    mempool
+};
